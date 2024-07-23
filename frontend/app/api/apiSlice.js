457fa990c1ade275ed590395fbe9741327c14aa1
simontamene.dev@gmail.com <simontamene.dev@gmail.com>
2024-07-23 19:28:39 +0300

    vercel modifyl

--- frontend/app/api/apiSlice.js
+++ frontend/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://next-js-14-with-express-and-mongodb-song-album-list-project.vercel.app/api' }),
   tagTypes: ['Song'],
   endpoints: (builder) => ({
     getSongs: builder.query({
